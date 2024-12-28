8ce91441c3610dec48cc3485d86efb28f1b5572e
DrecQ <drecquist@gmail.com>
2024-12-28 15:27:19 +0100

    [Chef] Creation des modèles laravel

---
+++ app/Http/Controllers/ElementsConstitutifsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ElementsConstitutifs;
+use Illuminate\Http\Request;
+
+class ElementsConstitutifsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ElementsConstitutifs $elementsConstitutifs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ElementsConstitutifs $elementsConstitutifs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ElementsConstitutifs $elementsConstitutifs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ElementsConstitutifs $elementsConstitutifs)
+    {
+        //
+    }
+}
