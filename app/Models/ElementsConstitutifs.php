46bf000cdb99f0209395a44ed5bd2d3a84949be5
DrecQ <drecquist@gmail.com>
2024-12-29 11:34:34 +0100

    [Chef] Creation des relations entre les UEs et les ECUes

--- app/Models/ElementsConstitutifs.php
+++ app/Models/ElementsConstitutifs.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ElementsConstitutifs extends Model
 {
-    //
+    protected $fillable = ['ue_id', 'code', 'nom', 'credits_ects'];
+
+    public function UnitesEnseignement()
+    {
+        return $this->belongsTo(UnitesEnseignement::class);
+    }
 }
--- app/Models/UnitesEnseignement.php
+++ app/Models/UnitesEnseignement.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UnitesEnseignement extends Model
 {
-    //
+    //Protection des données
+    protected $fillable = ['code', 'nom'];
+
+    public function ElementsConstitutifs()
+    {
+        return $this->hasMany(ElementsConstitutifs::class);
+    }
 }
