7e30df5e7b189da30908f3af36b34987f6f9fb1e
DrecQ <drecquist@gmail.com>
2025-01-03 06:50:14 +0100

    [Chef] Test pour la creation d'une UE valide, creation des factories

---
+++ database/factories/ElementsConstitutifsFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UnitesEnseignement;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ElementsConstitutifs>
+ */
+class ElementsConstitutifsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = UnitesEnseignement::class;
+
+    public function definition(): array
+    {
+        return [
+            'code' => 'UE'.$this->faker->unique()->numberBetween(001,999),
+            'nom' => $this->faker->sentence(3),
+            'credits_ects' => $this->faker->numberBetween(1, 30),
+            'semestre' => $this->faker->numberBetween(1, 6)
+        ];
+    }
+}
---
+++ tests/Feature/UETest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\UnitesEnseignement;
+
+
+class UETest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_creation_ue_valide()
+    {
+        $ue = UnitesEnseignement::factory()->create([
+            'code' => 'UE11',
+            'nom' => 'Programmation Web',
+            'credits_ects' => 6,
+            'semestre' => 1
+        ]);
+
+        $this->assertDatabaseHas('unites_enseignements', [
+            'code' => 'UE11',
+            'nom' => 'Programmation Web',
+            'credits_ects' => 6,
+            'semestre' => 1
+        ]);
+    }
+}
